d3930c11e2ce02308df01c3f83217185d3394135
xeekseven <xeekseven@outlook.com>
2019-08-10 23:24:37 +0800

    基于声明的授权demo提交

---
+++ ANC-Authorize-Policy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ANC_Authorize_Policy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ANC-Authorize-Policy/Startup.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ANC_Authorize_Policy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddAuthorization(options =>
+            {
+                //声明一个名为Administrator的授权，判断角色的claimType为Role的值是否equal Administrator
+                options.AddPolicy("Administrator", policy => policy.RequireClaim("Role","Administrator"));
+            });
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.AccessDeniedPath = new PathString("/Home/NotPermission");
+                options.LoginPath = new PathString("/Home/Login");
+                options.ExpireTimeSpan = TimeSpan.FromSeconds(10);
+            });
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            
+            app.UseAuthentication();
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseCookiePolicy();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
